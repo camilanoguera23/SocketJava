08987a8011f9a9a6110f607a061d4ad303c5d230
camilanoguera23 <camilanoguera23@gmial.com>
2020-02-19 20:01:04 -0500

    Create class connection

--- src/services/Connection.java
+++ src/services/Connection.java
@@ -1,5 +1,28 @@
 package services;
 
-public class Connection {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Connection {
+ 
+	private final int PORT = 8090;
+	private final String HOST= "localhost";
+	
+	protected ServerSocket ss;
+	protected Socket s;
+	
+	protected DataOutputStream outputServer, outputClient;
+	
+	public Connection(String type) throws IOException {
+		if (type.equalsIgnoreCase("server")) {
+			ss= new ServerSocket(PORT);
+			s= new Socket();
+		}
+		else {
+			s=new Socket(HOST, PORT);
+		}
+	}
+	
 }
